cdaca4a30a945eadcfb97c0979bf3b66e8c61295
jcanaviri42 <jcanaviri42@gmail.com>
2023-04-22 07:33:06 -0400

    changes in twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -8,9 +8,8 @@ const twoSum = (nums, target) => {
     // compute the complement of the current element
     const complement = target - nums[i]
     // if the complement is already in the hash map, return its index and the current index
-    if (map.has(complement)) {
-      return [map.get(complement), i]
-    }
+    if (map.has(complement)) return [map.get(complement), i]
+
     // otherwise, add the current element to the hash map with its index
     map.set(nums[i], i)
   }
@@ -19,8 +18,7 @@ const twoSum = (nums, target) => {
   return []
 }
 
-const nums = [2, 7, 11, 15];
-const target = 9;
-const result = twoSum(nums, target);
-console.log(result); // output: [0, 1]
-
+const nums = [2, 7, 11, 15]
+const target = 9
+const result = twoSum(nums, target)
+console.log(result) // output: [0, 1]
